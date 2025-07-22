cb2d076e024d91d71fc6ba575fe34d0e615a878f
harrshalll <havhad143@gmail.com>
2025-07-22 21:52:46 +0530

    class and objects

--- src/com/harshal/introduction/Main.java
+++ src/com/harshal/introduction/Main.java
@@ -1,4 +1,5 @@
 package com.harshal.introduction;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,8 +15,16 @@ public class Main {
         float[] marks = new float[5];
 
 
-        //new object from class student
-        student s1;
+        // creating new object from class student
+        student student1 = new student();//The (.) (dot) operator is used to access members of a class through an object.
+        student1.rno = 54;// new keyword---> used to dynamically allocates memory and returns a ref.variable to it
+        student1.name = "Harshal";
+        student1.marks = 8.41f;
+
+        //creating new objects in an array
+        student[] students = new student[5];
+        student student2 = new student();
+
     }
 }
 //create a class---> class is a named group of properties and methods.
@@ -29,8 +38,13 @@ class student {
 1. here we create a class which has three properties rno, names and marks.
 2. If you want to create your own data type then it is done by creating new class.
 3. class is a template of object.
-4.  An object is an instance created from the class template, containing actual data.
+4. An object is an instance created from the class template, containing actual data.
 5. class ---> logical construct (does not present in reality)
 6. object ---> physical reality (This is something which occupies space in memory.)
+7. Properties of an Objects.
+ Term           Meaning
+ State--->      The data/properties (fields/variables) of the object
+ Identity-->    The unique reference/address that differentiates one object from another
+ Behavior-->    The actions the object can perform (methods/functions)
 
 */
