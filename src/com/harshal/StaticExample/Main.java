124d493b6afbe6130667e6029710ab96cbf65005
harrshalll <havhad143@gmail.com>
2025-08-05 18:41:46 +0530

    Static and Non-Static

--- src/com/harshal/StaticExample/Main.java
+++ src/com/harshal/StaticExample/Main.java
@@ -15,9 +15,19 @@ public class Main {
 //        System.out.println(Harshal.population);
 //        System.out.println(Arpit.population);
 
-        // how can above convention work? ---> because compiler first check does the object have the property if it's not
+        // how can above convention works? ---> because compiler first check does the object have the property if it's not
         // it directed to the class check there. if it is there then it will print.
 
+//         greeting(); you can not access non-static inside static field because it requires an instances
 
+        // here is how can you access non-static inside static
+        Main obj = new Main();
+        obj.greeting();
+    }
+    // we know that something which is not static belongs to an object
+    void greeting(){
+        System.out.println("Hello!");
     }
 }
+// You cannot use non-static inside  without creating an object and accessing via object, but you can use static inside
+// non-static because it doesn't depend on objects
