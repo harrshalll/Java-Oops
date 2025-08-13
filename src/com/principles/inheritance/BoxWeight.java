79c4e4b5fe1f635fc0ad1b06c54f0f84ff862f91
harrshalll <havhad143@gmail.com>
2025-08-13 17:39:44 +0530

    Explanation added

--- src/com/principles/inheritance/BoxWeight.java
+++ src/com/principles/inheritance/BoxWeight.java
@@ -11,5 +11,7 @@ public class BoxWeight extends Box{
         super(h, l, w);// what is this?--> call the parent class constructor
         // used to initialize values present in parent class.
         this.weight = weight;
+        // In subclass/ child class you have to call the parent class constructor first by super(parameters);
+        //super(h, l, w); is not allowed, because compiler will see from top to bottom while compiling.
     }
 }
--- src/com/principles/inheritance/Main.java
+++ src/com/principles/inheritance/Main.java
@@ -25,9 +25,10 @@ public class Main {
         //hence you should have access to weight variable
         //this is also means that the ones you are trying to access should be initialized
         // but here, when obj itself is of type parent class, how will you call the constructor who can access the weight field.
+        //in short, when constructor of Box is being called when you create an object and Box constructor has three parameters only.
         //parent class has no idea about weight field of child class so you cannot initialized weight in obj of type Box class
-//        BoxWeight box6 = new Box(2,5,6);
-//        System.out.println(box6.weight);
+//        BoxWeight box5 = new Box(2,5,6);
+//        System.out.println(box5.weight);
 
 
     }
@@ -39,7 +40,7 @@ Note:-
 2. parent class is also known as superclass and child class also known as subclass
 3. super keyword is to access the attributes of superclass.
 4. If you have two weight attributes i.e. one in superclass and another in child class ,and you want to access the weight
-   attribute from superclass(field hiding), super keyword is used there. Note 3&4 is use case of super keyword
+   attribute from superclass(field hiding) by super.weight, super keyword is used there. Note 3&4 is use case of super keyword
 5. The 'super' keyword is used to:
    - Access members (fields or methods) of the superclass.
    - Call the superclass constructor from the subclass constructor.
